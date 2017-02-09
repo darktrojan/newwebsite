4a4e0844d9641c516323a5d1c10b8ebee317f31a
Geoff Lankow <geoff@darktrojan.net>
2017-02-09 22:07:11 +1300

    Refactor link and image pickers

--- website/content/static/admin/content/edit_dialogs.js
+++ website/content/static/admin/content/edit_dialogs.js
@@ -1,84 +1,168 @@
-var imageareabg = document.getElementById('imageareabg');
-imageareabg.onclick = function() {
-	imageareabg.style.display = null;
-	imagearea.style.display = null;
-	linkarea.style.display = null;
+/* globals Edit */
+/* jshint -W004 */
+var PickerBackground = {
+	area: document.getElementById('pickerbackground'),
+	show: function() {
+		this.area.style.display = null;
+	},
+	hide: function() {
+		this.area.style.display = 'none';
+	}
 };
-var imagearea = document.getElementById('imagearea');
-var inner = imagearea.querySelector('div');
-fetch('/admin/content/all_images', {credentials: 'same-origin'}).then(function(result) {
-	return result.json();
-}).then(function(json) {
-	function show_folder(folder, parent) {
-		var folderinner = document.createElement('div');
-		folderinner.classList.add('folderinner');
-		folderinner.dataset.dirname = folder.path;
-		inner.appendChild(folderinner);
+var ImagePicker = {
+	loaded: false,
+	area: document.getElementById('imagearea'),
 
-		if (parent) {
-			var div = document.createElement('div');
-			var a = document.createElement('a');
-			a.href = '#';
-			a.classList.add('folder');
-			a.dataset.path = parent.path;
-			a.textContent = ' Parent folder';
-			var span = document.createElement('span');
-			span.classList.add('folder-icon');
-			a.insertBefore(span, a.firstChild);
-			div.appendChild(a);
-			folderinner.appendChild(div);
-			folderinner.setAttribute('hidden', '');
-		}
-		for (var f of folder.folders) {
-			var div = document.createElement('div');
-			var a = document.createElement('a');
-			a.href = '#';
-			a.classList.add('folder');
-			a.dataset.path = f.path;
-			a.textContent = ' ' + f.path.substring(f.path.lastIndexOf('/') + 1);
-			var span = document.createElement('span');
-			span.classList.add('folder-icon');
-			a.insertBefore(span, a.firstChild);
-			div.appendChild(a);
-			folderinner.appendChild(div);
-		}
-		for (var i of folder.images) {
-			var div = document.createElement('div');
-			div.classList.add('image');
-			div.dataset.src = i.url;
-			div.dataset.dirname = i.url.substring(14, i.url.lastIndexOf('/'));
-			div.title = i.url.substring(i.url.lastIndexOf('/') + 1);
-			var img = document.createElement('img');
-			if (i.thumb) {
-				img.src = i.thumb;
-			} else {
-				img.onload = load_next_thumb;
+	init: function() {
+		this.inner = this.area.querySelector('div');
+		this.form = this.area.querySelector('form');
+		this.thumbnail = this.form.querySelector('img');
+		this.nameLabel = this.form.querySelector('div');
+		this.submitButton = this.form.querySelector('button');
+	},
+	load: function() {
+		this.loaded = true;
+		fetch('/admin/content/all_images', {credentials: 'same-origin'}).then(function(result) {
+			return result.json();
+		}).then(function(json) {
+			function show_folder(folder, parentFolder) {
+				var folderinner = document.createElement('div');
+				folderinner.classList.add('folderinner');
+				folderinner.dataset.dirname = folder.path;
+				ImagePicker.inner.appendChild(folderinner);
+
+				if (parentFolder) {
+					var div = document.createElement('div');
+					var a = document.createElement('a');
+					a.href = '#';
+					a.classList.add('folder');
+					a.dataset.path = parentFolder.path;
+					a.textContent = ' Parent folder';
+					var span = document.createElement('span');
+					span.classList.add('folder-icon');
+					a.insertBefore(span, a.firstChild);
+					div.appendChild(a);
+					folderinner.appendChild(div);
+					folderinner.setAttribute('hidden', '');
+				}
+				for (var f of folder.folders) {
+					var div = document.createElement('div');
+					var a = document.createElement('a');
+					a.href = '#';
+					a.classList.add('folder');
+					a.dataset.path = f.path;
+					a.textContent = ' ' + f.path.substring(f.path.lastIndexOf('/') + 1);
+					var span = document.createElement('span');
+					span.classList.add('folder-icon');
+					a.insertBefore(span, a.firstChild);
+					div.appendChild(a);
+					folderinner.appendChild(div);
+				}
+				for (var i of folder.images) {
+					var div = document.createElement('div');
+					div.classList.add('image');
+					div.dataset.src = i.url;
+					div.dataset.dirname = i.url.substring(14, i.url.lastIndexOf('/'));
+					div.title = i.url.substring(i.url.lastIndexOf('/') + 1);
+					var img = document.createElement('img');
+					if (i.thumb) {
+						if (parentFolder) {
+							div.dataset.thumb = i.thumb;
+						} else {
+							img.src = i.thumb;
+						}
+					} else {
+						img.onload = ImagePicker.load_next_thumb;
+					}
+					div.appendChild(img);
+					folderinner.appendChild(div);
+				}
+				for (var f of folder.folders) {
+					show_folder(f, folder);
+				}
 			}
-			div.appendChild(img);
-			folderinner.appendChild(div);
-		}
-		for (var f of folder.folders) {
-			show_folder(f, folder);
-		}
-	}
 
-	show_folder(json);
-	load_next_thumb();
-});
-function load_next_thumb() {
-	var next = inner.querySelector('div.folderinner:not([hidden]) img:not([src])');
-	if (next) {
-		var name = next.parentNode.dataset.src.substring(7); // length of '/media/'
-		next.src = '/admin/content/get_thumbnail?f=' + encodeURIComponent(name);
+			show_folder(json);
+			ImagePicker.load_next_thumb();
+		});
+	},
+	load_next_thumb: function() {
+		var next = ImagePicker.inner.querySelector('div.folderinner:not([hidden]) img:not([src])');
+		if (next) {
+			var data = next.parentNode.dataset;
+			if (data.thumb) {
+				next.src = data.thumb;
+				ImagePicker.load_next_thumb();
+			} else {
+				var name = data.src.substring(7); // length of '/media/'
+				next.src = '/admin/content/get_thumbnail?f=' + encodeURIComponent(name);
+			}
+		}
+	},
+	show: function() {
+		PickerBackground.show();
+		this.area.style.display = null;
+		this.inner.focus();
+
+		if (!this.loaded) {
+			this.load();
+		}
+	},
+	hide: function() {
+		this.area.style.display = 'none';
+		PickerBackground.hide();
 	}
-}
-imagearea.onclick = function(event) {
+};
+var LinkPicker = {
+	loaded: false,
+	area: document.getElementById('linkarea'),
+
+	init: function() {
+		this.form = this.area.querySelector('form');
+		this.submitButton = this.form.querySelector('button');
+		this.list = this.area.querySelector('select');
+	},
+	load: function() {
+		fetch('/admin/content/all_pages', {credentials: 'same-origin'}).then(function(result) {
+			return result.json();
+		}).then(function(json) {
+			for (var i of json) {
+				var div = document.createElement('option');
+				div.classList.add('link');
+				div.value = i.url;
+				div.textContent = i.title;
+				LinkPicker.list.appendChild(div);
+			}
+		});
+	},
+	show: function() {
+		PickerBackground.show();
+		this.area.style.display = null;
+		this.list.focus();
+
+		if (!this.loaded) {
+			this.load();
+		}
+	},
+	hide: function() {
+		this.area.style.display = 'none';
+		PickerBackground.hide();
+	}
+};
+
+PickerBackground.area.onclick = function() {
+	ImagePicker.hide();
+	LinkPicker.hide();
+};
+
+ImagePicker.init();
+ImagePicker.area.onclick = function(event) {
 	var target = event.target;
 	if (target instanceof HTMLImageElement) {
 		target = target.parentNode;
 	}
 	if (target instanceof HTMLAnchorElement && target.classList.contains('folder')) {
-		var folders = imagearea.querySelectorAll('div.folderinner');
+		var folders = ImagePicker.area.querySelectorAll('div.folderinner');
 		for (var i of folders) {
 			if (i.dataset.dirname == target.dataset.path) {
 				i.removeAttribute('hidden');
@@ -86,27 +170,25 @@ imagearea.onclick = function(event) {
 				i.setAttribute('hidden', '');
 			}
 		}
-		load_next_thumb();
+		ImagePicker.load_next_thumb();
 		return false;
 	} else if (target instanceof HTMLDivElement && target.classList.contains('image')) {
-		var selected = imagearea.querySelector('div.image.selected');
+		var selected = ImagePicker.area.querySelector('div.image.selected');
 		if (selected) {
 			selected.classList.remove('selected');
 		}
 		target.classList.add('selected');
-		var thumb = imageform.querySelector('img');
-		thumb.onload = function() {
-			imageform.width.value = this.naturalWidth;
-			imageform.height.value = this.naturalHeight;
+		ImagePicker.thumbnail.onload = function() {
+			ImagePicker.form.width.value = this.naturalWidth;
+			ImagePicker.form.height.value = this.naturalHeight;
+			ImagePicker.thumbnail.onload = null;
 		};
-		thumb.src = imageform.src.value = target.dataset.src;
-		var name = imageform.querySelector('div');
-		name.textContent = target.dataset.src.substring(target.dataset.src.lastIndexOf('/') + 1);
-		imageform.querySelector('button').disabled = false;
+		ImagePicker.thumbnail.src = ImagePicker.form.src.value = target.dataset.src;
+		ImagePicker.nameLabel.textContent = target.dataset.src.substring(target.dataset.src.lastIndexOf('/') + 1);
+		ImagePicker.submitButton.disabled = false;
 	}
 };
-var imageform = imagearea.querySelector('form');
-imageform.onsubmit = function() {
+ImagePicker.form.onsubmit = function() {
 	try {
 		Edit.Actions.imageAction({
 			src: this.src.value, width: this.width.value, height: this.height.value
@@ -118,65 +200,49 @@ imageform.onsubmit = function() {
 		return false;
 	}
 };
-imageform.onreset = function() {
-	imageareabg.style.display = null;
-	imagearea.style.display = null;
-	var selected = imagearea.querySelector('div.image.selected');
+ImagePicker.form.onreset = function() {
+	ImagePicker.hide();
+	var selected = ImagePicker.area.querySelector('div.image.selected');
 	if (selected) {
 		selected.classList.remove('selected');
 	}
-	var thumb = imageform.querySelector('img');
-	thumb.src = '';
-	var name = imageform.querySelector('div');
-	name.textContent = null;
-	imageform.reset();
-	imageform.querySelector('button[type="submit"]').disabled = true;
+	ImagePicker.thumbnail.src = '';
+	ImagePicker.nameLabel.textContent = null;
+	ImagePicker.form.reset();
+	ImagePicker.submitButton.disabled = true;
 	Edit.currentWindow.focus();
 };
-imageform.width.oninput = function() {
-	var thumb = imageform.querySelector('img');
-	imageform.height.value = Math.round(this.value / thumb.naturalWidth * thumb.naturalHeight);
+ImagePicker.form.width.oninput = function() {
+	ImagePicker.form.height.value = Math.round(
+		this.value / ImagePicker.thumbnail.naturalWidth * ImagePicker.thumbnail.naturalHeight
+	);
 };
-imageform.height.oninput = function() {
-	var thumb = imageform.querySelector('img');
-	imageform.width.value = Math.round(this.value / thumb.naturalHeight * thumb.naturalWidth);
+ImagePicker.form.height.oninput = function() {
+	ImagePicker.form.width.value = Math.round(
+		this.value / ImagePicker.thumbnail.naturalHeight * ImagePicker.thumbnail.naturalWidth
+	);
 };
+
 Edit.imageCallback = function(existing) {
-	imageareabg.style.display = 'block';
-	imagearea.style.display = 'flex';
-	imagearea.querySelector('div').focus();
+	ImagePicker.show();
 
 	if (existing) {
-		var thumb = imageform.querySelector('img');
-		thumb.src = imageform.src.value = existing.getAttribute('src');
-		imageform.width.value = existing.getAttribute('width');
-		imageform.height.value = existing.getAttribute('height');
-		imageform.querySelector('button').disabled = false;
-		imageform.querySelector('button').textContent = 'Update';
+		ImagePicker.thumbnail.src = ImagePicker.form.src.value = existing.getAttribute('src');
+		ImagePicker.form.width.value = existing.getAttribute('width');
+		ImagePicker.form.height.value = existing.getAttribute('height');
+		ImagePicker.submitButton.disabled = false;
+		ImagePicker.submitButton.textContent = 'Update';
 	} else {
-		imageform.querySelector('button').textContent = 'Insert';
+		ImagePicker.submitButton.textContent = 'Insert';
 	}
 };
 
-var linkarea = document.getElementById('linkarea');
-fetch('/admin/content/all_pages', {credentials: 'same-origin'}).then(function(result) {
-	return result.json();
-}).then(function(json) {
-	var list = linkarea.querySelector('select');
-	for (var i of json) {
-		var div = document.createElement('option');
-		div.classList.add('link');
-		div.value = i.url;
-		div.textContent = i.title;
-		list.appendChild(div);
-	}
-	list.onchange = function() {
-		linkform.href.value = this.value;
-		linkform.querySelector('button').disabled = false;
-	};
-});
-var linkform = linkarea.querySelector('form');
-linkform.onsubmit = function() {
+LinkPicker.init();
+LinkPicker.list.onchange = function() {
+	LinkPicker.form.href.value = this.value;
+	LinkPicker.submitButton.disabled = false;
+};
+LinkPicker.form.onsubmit = function() {
 	try {
 		Edit.Actions.linkAction({
 			href: this.href.value,
@@ -189,17 +255,15 @@ linkform.onsubmit = function() {
 		return false;
 	}
 };
-linkform.onreset = function() {
-	imageareabg.style.display = null;
-	linkarea.style.display = null;
-	linkarea.querySelector('select').selectedIndex = -1;
-	linkform.querySelector('button[type="submit"]').disabled = true;
+LinkPicker.form.onreset = function() {
+	LinkPicker.hide();
+	LinkPicker.list.selectedIndex = -1;
+	LinkPicker.submitButton.disabled = true;
 	setTimeout(function() {
 		Edit.currentWindow.focus();
 	}, 0);
 };
+
 Edit.linkCallback = function() {
-	imageareabg.style.display = 'block';
-	linkarea.style.display = 'flex';
-	linkarea.querySelector('select').focus();
+	LinkPicker.show();
 };
